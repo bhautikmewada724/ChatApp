4620a4836e80ea0f903c4bf04eeb1b979677151f
bhautikmewada724 <120450397+bhautikmewada724@users.noreply.github.com>
2025-05-04 18:16:00 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -30,7 +30,7 @@ function Signup() {
     };
     // console.log(userInfo);
     await axios
-      .post("/api/user/signup", userInfo)
+      .post("https://chatapp-backened-r6dr.onrender.com/user/signup", userInfo)
       .then((response) => {
         if (response.data) {
           toast.success("Signup successful");
